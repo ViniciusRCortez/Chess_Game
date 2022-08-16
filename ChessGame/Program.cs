750b45c33b92d8bcaaaea8b87895593e7deab67f
Vinícius Cortez <73963766+ViniciusRCortez@users.noreply.github.com>
2022-08-16 16:46:48 -0300

    Começando a imprimir tabuleiro

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -7,10 +7,10 @@ namespace ChessGame
     {
         private static void Main(string[] args)
         {
-            Posicao P = new Posicao(3, 4);
-            Console.WriteLine(P);
+
 
             Tabuleiro T = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(T);
         }
     }
 }
