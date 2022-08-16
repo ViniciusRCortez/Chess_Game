4e1a0ea5ac45c8441cf735a58931fbef2246e9c4
Vinícius Cortez <73963766+ViniciusRCortez@users.noreply.github.com>
2022-08-16 16:31:40 -0300

    Começando a implementar tabuleiro

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,4 +1,4 @@
-﻿using ChessGame.Tabuleiro;
+﻿using ChessGame.tabuleiro;
 
 
 namespace ChessGame
@@ -9,6 +9,8 @@ namespace ChessGame
         {
             Posicao P = new Posicao(3, 4);
             Console.WriteLine(P);
+
+            Tabuleiro T = new Tabuleiro(8, 8);
         }
     }
 }
---
+++ ChessGame/Tabuleiro/Cor.cs
@@ -0,0 +1,13 @@
+﻿namespace ChessGame.tabuleiro
+{
+    enum Cor
+    {
+        Preta,
+        Branca,
+        Laranja,
+        Amarelo,
+        Azul,
+        Vermelho,
+        Verde
+    }
+}
